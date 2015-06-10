3d8728762da30525b2ddca6ebdca28ed0c149f20
Robin Kanters <robin@robinkanters.nl>
2015-06-10 20:13:45 +0200

    Created unit tests for Nhtsdo API requests

---
+++ SnoMedVisualiser.UnitTest/Nhtsdo/NhtsdoClientTest.cs
@@ -0,0 +1,22 @@
+﻿namespace SnoMedVisualiser.UnitTest.Nhtsdo
+{
+    using Interface;
+    using Interface.Fakes;
+    using Model;
+    using NUnit.Framework;
+    using SnoMedVisualiser.Nhtsdo;
+
+    public class NhtsdoClientTest
+    {
+        [Test]
+        public void NhtsdoClient_DoApiRequest_Success()
+        {
+            var id = SctId.FromString("41796003");
+            var answer = NhtsdoClient.LookupAnswer(id);
+
+            Assert.That(answer, Is.Not.Null);
+            Assert.That(answer.SctId.Id, Is.EqualTo(id.Id));
+            Assert.That(answer.DefaultTerm, Is.Not.Null.And.Not.Empty);
+        }
+    }
+}
