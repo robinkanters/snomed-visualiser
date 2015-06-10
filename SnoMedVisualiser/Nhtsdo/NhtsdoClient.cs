19582fd3d19267a84fd6d77746460fc7ed1e6a5a
Robin Kanters <robin@robinkanters.nl>
2015-06-10 20:23:11 +0200

    Implement Async Api request

--- SnoMedVisualiser/Nhtsdo/NhtsdoClient.cs
+++ SnoMedVisualiser/Nhtsdo/NhtsdoClient.cs
@@ -4,6 +4,7 @@
     using System.IO;
     using System.Net;
     using System.Threading;
+    using System.Threading.Tasks;
     using Interface;
     using Model;
 
@@ -13,6 +14,13 @@
         {
             var request = BuildApiRequest(sctId);
             var apiResult = MakeApiCall(request);
+            return ParseApiResult(apiResult);
+        }
+
+        public static async Task<ISnomedAnswer> LookupAnswerAsync(ISctId sctId)
+        {
+            var request = BuildApiRequest(sctId);
+            var apiResult = await MakeApiCallAsync(request);
             return ParseApiResult(apiResult);
         }
 
@@ -31,6 +39,24 @@
         private static INhtsdoClientResponse MakeApiCall(INhtsdoClientRequest request)
         {
             var response = request.WebRequest.GetResponse();
+            var reader = new StreamReader(response.GetResponseStream());
+
+            var responseString = reader.ReadToEnd();
+
+            reader.Close();
+            reader.Dispose();
+            response.Close();
+            response.Dispose();
+
+            return new NhtsdoClientResponse
+            {
+                Response = responseString
+            };
+        }
+
+        private static async Task<INhtsdoClientResponse> MakeApiCallAsync(INhtsdoClientRequest request)
+        {
+            var response = await request.WebRequest.GetResponseAsync();
             var reader = new StreamReader(response.GetResponseStream());
 
             var responseString = reader.ReadToEnd();
